7851c0da33193a501de336d016440c6e9ddf4268
stepan9773 <stepan9773@users.noreply.github.com>
2021-01-18 22:02:44 +0200

    editor page code js isolation

--- UIArtify/wwwroot/js/isolated/EditorToolbar.razor.js
+++ UIArtify/wwwroot/js/isolated/EditorToolbar.razor.js
@@ -1,5 +1,5 @@
 ﻿
-function navSlide(){
+export function navSlide(){
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li')
