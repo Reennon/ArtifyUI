b8b6ec7df404241667e31533adb5177642f39e41
Roman Kovalchuk <37474734+Reennon@users.noreply.github.com>
2021-01-18 22:47:35 +0200

    JS Isolated

--- UIArtify/wwwroot/js/isolated/NavBackground.razor.js
+++ UIArtify/wwwroot/js/isolated/NavBackground.razor.js
@@ -1,1 +1,16 @@
-﻿console.log("NavBackground.razor.js is loaded!");
+﻿console.log("NavBackgroun7d.razor.js is loaded!");
+console.log("NavBackground.razor.js is loaded!");
+
+export function empty(name) {
+    console.log("Empty");
+    console.log(name);
+    document.getElementsByClassName("rose")[0].style = "background-color: red;\n" +
+        "width: 100px;\n" +
+        "height: 100px;\n" +
+        "position: absolute;";
+    document.getElementsByClassName("rose")[1].style = "background-color: red;\n" +
+        "width: 100px;\n" +
+        "height: 100px;\n" +
+        "position: absolute;";
+    console.log(document.documentElement.innerHTML)
+}
