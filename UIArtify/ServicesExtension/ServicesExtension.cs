43033805c56dce5ca95fb7c827b7aeb3f5b6588a
Roman Kovalchuk <37474734+Reennon@users.noreply.github.com>
2021-02-03 22:48:36 +0200

    some

--- UIArtify/ServicesExtension/ServicesExtension.cs
+++ UIArtify/ServicesExtension/ServicesExtension.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using BlazorDownloadFile;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using UIArtify.Configurations;
+using UIArtify.Interfaces;
+using UIArtify.Services;
 
 namespace UIArtify.ServicesExtension
 {
     public static class ServicesExtension
     {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IQueryService, QueryService>();
+            services.AddBlazorDownloadFile();
+            return services;
+        }
         public static void InitializeConfigurations(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<Api>(configuration.GetSection("Api"));
