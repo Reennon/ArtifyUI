0cafa73d3c642d7c5ba23f4e827cab4e5ec559ee
YuriiChernii <mikeke373737@gmail.com>
2021-02-06 19:32:41 +0200

    rel-2

--- UIArtify/Models/Account/AddUser.cs
+++ UIArtify/Models/Account/AddUser.cs
@@ -4,16 +4,16 @@ namespace UIArtify.Models.Account
 {
     public class AddUser
     {
-        [Required]
+        [Required(ErrorMessage = "The Email field is required")]
         public string FirstName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "The Login field is required")]
         public string LastName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "The Password field is required")]
         public string Username { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "The Password field is required")]
         [MinLength(6, ErrorMessage = "The Password field must be a minimum of 6 characters")]
         public string Password { get; set; }
     }
