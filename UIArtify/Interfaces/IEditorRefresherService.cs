cf72cb8cef5cece90fb26cefb55dd3c44103896d
Roman Kovalchuk <37474734+Reennon@users.noreply.github.com>
2021-02-21 10:10:26 +0200

    Develop/query code analysis (#20)
    * initial commit
    
    * Query is ready

---
+++ UIArtify/Interfaces/IEditorRefresherService.cs
@@ -0,0 +1,11 @@
+﻿using BlazorMonaco;
+
+namespace UIArtify.Interfaces
+{
+    public interface IEditorRefresherService
+    {
+        void AddEditor(MonacoEditor editor);
+        
+        void RefreshEditor();
+    }
+}
